05aa4cae67f2f5ab1d57981aca257aa2fc164fb7
Nikit Hamal <iamnikithamal@gmail.com>
2026-05-27 16:16:20 +0545

    fix: add missing Google Font import in Type.kt

--- app/src/main/java/com/neb/ians/ui/theme/Type.kt
+++ app/src/main/java/com/neb/ians/ui/theme/Type.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import com.neb.ians.R
