fde65d641967df377701894051fac484fed24053
Nikit Hamal <iamnikithamal@gmail.com>
2026-05-27 15:42:23 +0545

    fix: correct signing configuration path and credentials

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,10 +25,10 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("${rootProject.projectDir}/nebians-release.keystore")
-            storePassword = "***REMOVED***"
+            storeFile = file("${rootProject.projectDir}/keystore/nebians-release.jks")
+            storePassword = "nebians2024"
             keyAlias = "nebians"
-            keyPassword = "***REMOVED***"
+            keyPassword = "nebians2024"
         }
     }
 
