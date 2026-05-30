024bbbef8f26dcef93472885f4653ae86ee95629
Nikit Hamal <iamnikithamal@gmail.com>
2026-05-30 23:02:48 +0545

    fix: add unsafe-eval to CSP script-src for PDF viewer embeds

--- backend_python/nebians/middleware.py
+++ backend_python/nebians/middleware.py
@@ -12,7 +12,7 @@ class SecurityHeadersMiddleware:
         response.setdefault(
             'Content-Security-Policy',
             "default-src 'self'; "
-            "script-src 'self' 'unsafe-inline' https://accounts.google.com https://www.gstatic.com; "
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://www.gstatic.com; "
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
             "font-src 'self' https://fonts.gstatic.com; "
             "img-src 'self' data: https:; "
