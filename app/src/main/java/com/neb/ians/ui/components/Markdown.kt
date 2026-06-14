28751a3cd97ae9753feee61f1e3726bb6d3b21a5
Nikit Hamal <iamnikithamal@gmail.com>
2026-06-14 17:11:56 +0545

    Import TextOverflow in Markdown.kt to resolve build error

--- app/src/main/java/com/neb/ians/ui/components/Markdown.kt
+++ app/src/main/java/com/neb/ians/ui/components/Markdown.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.text.ClickableText
