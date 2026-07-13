95756d9dde546b795418d52623be662460496a4d
Nikit Hamal <iamnikithamal@gmail.com>
2026-07-13 18:22:43 +0545

    ip

--- backend_python/web/view_helpers.py
+++ backend_python/web/view_helpers.py
@@ -34,7 +34,7 @@ from api import counters as _counters
 from api import notifications as _notif
 from api import cleanup as _cleanup
 from api import realtime as _rt
-from api.view_helpers import map_profile_grade
+from api.view_helpers import map_profile_grade, is_global_user
 from . import api_client as api
 
 logger = logging.getLogger(__name__)
