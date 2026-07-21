f5cf246324ae0f141754f96768de0961dbc6d8a4
Nikit Hamal <iamnikithamal@gmail.com>
2026-07-21 12:55:01 +0545

    up

--- app/src/main/java/com/neb/ians/ui/screens/resource/ResourceDetailScreen.kt
+++ app/src/main/java/com/neb/ians/ui/screens/resource/ResourceDetailScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
