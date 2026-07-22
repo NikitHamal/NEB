92e9ed65b7510d88c91c3af40202cd1ca0db08df
Nikit Hamal <iamnikithamal@gmail.com>
2026-07-22 09:15:02 +0545

    Upgrade targetSdk and compileSdk to 36 (Android 16), bump version to 1.6.0 (versionCode 22)

--- admin/build.gradle.kts
+++ admin/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 
 android {
     namespace = "com.neb.ians.admin"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     defaultConfig {
         applicationId = "com.neb.ians.admin"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- consica-code/app/build.gradle.kts
+++ consica-code/app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.consica.code"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.consica.code"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
