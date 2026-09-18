e1b7eb188e7237873d7af15318ba18dfef038673
NEB Deploy <deploy@nebians.com>
2026-09-18 14:15:52 +0545

    AgentX: add missing contentOrNull import in RoutineRunner

--- agentx/src/main/java/com/agentx/app/data/tools/RoutineRunner.kt
+++ agentx/src/main/java/com/agentx/app/data/tools/RoutineRunner.kt
@@ -10,6 +10,7 @@ import javax.inject.Singleton
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.buildJsonArray
 import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
